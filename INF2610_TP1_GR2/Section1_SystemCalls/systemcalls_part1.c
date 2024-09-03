3c4487d95613f6cbadbd49b00a4219c4839f3a72
NoelaPanier <noela.panier@polymtl.ca>
2024-09-03 16:48:05 -0400

    fichier modifier

--- INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
+++ INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
@@ -6,8 +6,24 @@
  * Ajoutez vos noms, prénoms et matricules
 */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main () {
+    int fd = open("systemcalls_output2.txt",O_WRONLY | O_TRUNC);
+    ssize_t bytesRead;
+    if (fd == -1){
+        printf("Appel système open a échoué");
+        exit(1);
+    }
+    printf("Saisissez votre texte suivi de CTRL-D :\n");
+    int fs = open(0, O_RDONLY);
+/*     char buffer;
+    while (buffer!="ˆD"){
+        read(fs,buffer,bytesRead);
+    } */
     // TODO
     return 0;
 }
