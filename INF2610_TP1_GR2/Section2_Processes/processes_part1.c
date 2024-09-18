cafe46aae581bdab9c8242ddf427e14ab3608a7c
thomasouvanlasy <thomasouvanlasy@gmail.com>
2024-09-18 11:13:16 -0400

    3.1 and 3.2

--- INF2610_TP1_GR2/Section2_Processes/processes_part1.c
+++ INF2610_TP1_GR2/Section2_Processes/processes_part1.c
@@ -12,6 +12,47 @@
 void question1()
 {
     // TODO
-    
+    registerProc(getpid(),getppid(),0,0);
+    if (fork() == 0) {// 1.1 
+        registerProc(getpid(),getppid(),1,1);
+        if(fork()==0){ // 2.1
+        registerProc(getpid(),getppid(),2,1);
+        }
+        wait(NULL);
+        _exit(0); 
+    }
+    if (fork() == 0) {// 1.2
+        registerProc(getpid(),getppid(),1,2);
+        if(fork()==0){ // 2.2
+        registerProc(getpid(),getppid(),2,2);
+        }
+        wait(NULL);
+        _exit(0); 
+    }
+    if (fork() == 0) {// 1.3
+        registerProc(getpid(),getppid(),1,3);
+
+        if(fork()==0){ // 2.3
+        registerProc(getpid(),getppid(),2,3);
+
+        _exit(0); 
+        }
+        if(fork()==0){ // 2.4
+        registerProc(getpid(),getppid(),2,4);
+        _exit(0); 
+        }
+        if(fork()==0){ // 2.5
+        registerProc(getpid(),getppid(),2,5);
+        _exit(0); 
+        }
+        if(fork()==0){ // 2.6
+        registerProc(getpid(),getppid(),2,6);
+        _exit(0); 
+        }
+        wait(NULL);wait(NULL);wait(NULL);wait(NULL);
+        _exit(0); 
+    }
+    while(wait(NULL)>0);
+    printProcRegistrations();
 }
 
