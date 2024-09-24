96d16121e749e756507ac146fcb72349b5dfc045
NoelaPanier <panier.noela@gmail.com>
2024-09-24 18:04:48 -0400

    revision question 3.2

--- INF2610_TP1_GR2/Section2_Processes/processes_part1.c
+++ INF2610_TP1_GR2/Section2_Processes/processes_part1.c
@@ -6,7 +6,7 @@
  * Ajoutez vos noms, prénoms et matricules
 */
 
-#include "libprocesslab/libprocesslab.h"
+#include "./libprocesslab/libprocesslab.h"
 
 
 void question1()
@@ -15,44 +15,51 @@ void question1()
     registerProc(getpid(),getppid(),0,0);
     if (fork() == 0) {// 1.1 
         registerProc(getpid(),getppid(),1,1);
+
         if(fork()==0){ // 2.1
-        registerProc(getpid(),getppid(),2,1);
+            registerProc(getpid(),getppid(),2,1);
+            _exit(0);
         }
-        wait(NULL);
+        while(wait(NULL)>0);
         _exit(0); 
     }
+
     if (fork() == 0) {// 1.2
         registerProc(getpid(),getppid(),1,2);
+
         if(fork()==0){ // 2.2
-        registerProc(getpid(),getppid(),2,2);
+            registerProc(getpid(),getppid(),2,2);
+            _exit(0);
         }
-        wait(NULL);
+        while(wait(NULL)>0);
         _exit(0); 
     }
+
     if (fork() == 0) {// 1.3
         registerProc(getpid(),getppid(),1,3);
 
         if(fork()==0){ // 2.3
-        registerProc(getpid(),getppid(),2,3);
-
-        _exit(0); 
+            registerProc(getpid(),getppid(),2,3);
+            _exit(0); 
         }
         if(fork()==0){ // 2.4
-        registerProc(getpid(),getppid(),2,4);
-        _exit(0); 
+            registerProc(getpid(),getppid(),2,4);
+            _exit(0); 
         }
         if(fork()==0){ // 2.5
-        registerProc(getpid(),getppid(),2,5);
-        _exit(0); 
+            registerProc(getpid(),getppid(),2,5);
+            _exit(0); 
         }
         if(fork()==0){ // 2.6
-        registerProc(getpid(),getppid(),2,6);
-        _exit(0); 
+            registerProc(getpid(),getppid(),2,6);
+            _exit(0); 
         }
-        wait(NULL);wait(NULL);wait(NULL);wait(NULL);
+        while(wait(NULL)>0);
         _exit(0); 
     }
-    while(wait(NULL)>0);
+    //while(wait(NULL)>0);
+    _exit(0);
     printProcRegistrations();
+    execl("/bin/ls", "ls","-l", NULL);
 }
 
