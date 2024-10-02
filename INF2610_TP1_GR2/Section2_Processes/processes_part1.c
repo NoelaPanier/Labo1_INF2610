689aab46a89a5ea1ad0acf75210c94deff0a200d
NoelaPanier <panier.noela@gmail.com>
2024-10-02 11:08:15 -0400

    3.3

--- INF2610_TP1_GR2/Section2_Processes/processes_part1.c
+++ INF2610_TP1_GR2/Section2_Processes/processes_part1.c
@@ -12,16 +12,23 @@
 void question1()
 {
     // TODO
+    int total = 0;
+    int value = 1;
+    int status;
     registerProc(getpid(),getppid(),0,0);
     if (fork() == 0) {// 1.1 
         registerProc(getpid(),getppid(),1,1);
-
+ 
         if(fork()==0){ // 2.1
             registerProc(getpid(),getppid(),2,1);
-            _exit(0);
+            _exit(value);
         }
-        while(wait(NULL)>0);
-        _exit(0); 
+        while(wait(&status)>0)
+        {
+            total += WEXITSTATUS(status);
+        }
+        _exit(total+1); 
+   
     }
 
     if (fork() == 0) {// 1.2
@@ -29,10 +36,13 @@ void question1()
 
         if(fork()==0){ // 2.2
             registerProc(getpid(),getppid(),2,2);
-            _exit(0);
+            _exit(value);
         }
-        while(wait(NULL)>0);
-        _exit(0); 
+         while(wait(&status)>0)
+        {
+            total += WEXITSTATUS(status);
+        }
+        _exit(total+1); 
     }
 
     if (fork() == 0) {// 1.3
@@ -40,24 +50,31 @@ void question1()
 
         if(fork()==0){ // 2.3
             registerProc(getpid(),getppid(),2,3);
-            _exit(0); 
+            _exit(value); 
         }
         if(fork()==0){ // 2.4
             registerProc(getpid(),getppid(),2,4);
-            _exit(0); 
+            _exit(value);
         }
         if(fork()==0){ // 2.5
             registerProc(getpid(),getppid(),2,5);
-            _exit(0); 
+            _exit(value);
         }
         if(fork()==0){ // 2.6
             registerProc(getpid(),getppid(),2,6);
-            _exit(0); 
+            _exit(value);
         }
-        while(wait(NULL)>0);
-        _exit(0); 
+         while(wait(&status)>0)
+        {
+            total += WEXITSTATUS(status);
+        }
+        _exit(total+1); 
     }
-    while(wait(NULL)>0);
+    while(wait(&status)>0)
+    {
+        total += WEXITSTATUS(status);
+    }
+    printf("Total des enfants directs et indirects du processus 0:  %d  \n", total);
     printProcRegistrations();
     execl("/bin/ls", "ls","-l", NULL);
 }
