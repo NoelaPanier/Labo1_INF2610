e62da52ac64ff153db4d0799184362d577b4b175
NoelaPanier <panier.noela@gmail.com>
2024-10-04 12:04:33 -0400

    changement 5.1

--- INF2610_TP1_GR2/Section3_Challenges/challenges_part1.c
+++ INF2610_TP1_GR2/Section3_Challenges/challenges_part1.c
@@ -13,31 +13,34 @@ int compter_fichiers_txt(const char *dirpath, FILE *fichier_sortie, long unsigne
     dirent *entree;
     char **fichiers_txt = malloc(MAX_PATH_LENGTH * sizeof(char *));
     char *chemin_print = "";
-    static long unsigned int *entree_d_ino_print = 0;
+    long unsigned int inode_actuel;
+   
 
     DIR *dir = opendir(dirpath);
     if( dir == NULL){
         perror("erreur à l'ouverture du dossier");
         return -1;
     }
-    
+
+
     while((entree = readdir(dir)) != NULL) {
         if (strcmp(entree->d_name, ".") == 0 || strcmp(entree->d_name, "..") == 0) {
             continue;
         }
+        inode_actuel = (long unsigned int)entree->d_ino;
 
         if(entree->d_type == 4){    // 4 c'est pour un repertoire
             pid_t pid;
             if((pid=fork()) == 0){    
                 char chemin_complet[MAX_PATH_LENGTH];
                 snprintf(chemin_complet, sizeof(chemin_complet), "%s/%s", dirpath, entree->d_name);
-                entree_d_ino_print = (long unsigned int*)entree->d_ino;
-
-                exit(compter_fichiers_txt(chemin_complet, fichier_sortie, (long unsigned int*) entree->d_ino));
+                //entree_d_ino_print = (long unsigned int*)entree->d_ino;
+                //inode_actuel = (long unsigned int)dirfd(dir);
+                exit(compter_fichiers_txt(chemin_complet, fichier_sortie, (long unsigned int*) inode_actuel));
             }
             else if(pid > 0){
                 int status;
-                entree_d_ino_print = (long unsigned int*)entree->d_ino;
+                //entree_d_ino_print = (long unsigned int*)entree->d_ino;
                 waitpid(pid, &status, 0);
                 if (WIFEXITED(status)){
                     nb_total_fichiers_txt += WEXITSTATUS(status);
@@ -59,7 +62,7 @@ int compter_fichiers_txt(const char *dirpath, FILE *fichier_sortie, long unsigne
     snprintf(chemin_complet, sizeof(chemin_complet), "%s", dirpath);
     fprintf(fichier_sortie, "-----------------------------------------------------------------\n");
     fprintf(fichier_sortie, "Emplacement du répertoire: %s\n", chemin_complet);
-    fprintf(fichier_sortie, "Numéro d'identification du répertoire: %llu\n",(unsigned long long)entree_d_ino_print);
+    fprintf(fichier_sortie, "Numéro d'identification du répertoire: %llu\n",(unsigned long long) inode_actuel);
     fprintf(fichier_sortie, "Numéro d'identification du répertoire supérieur immédiat: %llu\n", (unsigned long long)parent_ino);
     fprintf(fichier_sortie, "Liste des fichiers texte: \n");
     if (nb_fichiers_txt == 0) {
@@ -87,7 +90,7 @@ int main(int argc, char*argv[])
         perror("Erreur à l'ouverture du fichier de sortie");
         return 1;
     }
-    int nb_total_fichiers_txt = compter_fichiers_txt("./root", fichier_sortie, NULL);
+    int nb_total_fichiers_txt = compter_fichiers_txt("./root copy", fichier_sortie, NULL);
     printf("Nombre total de fichiers texte: %d\n", nb_total_fichiers_txt);
 
     fclose(fichier_sortie);
